d1cf2e57ac6f54a962b761088d01ef59d67e2068
Mirian Fonkam <mirian.fonkam@aun.edu.ng>
2021-07-23 10:54:48 -0300

    commit MoviesResponse

---
+++ app/src/main/java/com/g/vicprojetointegrador/data/model/MoviesResponse.kt
@@ -0,0 +1,11 @@
+package com.g.vicprojetointegrador.data.model
+
+import com.google.gson.annotations.SerializedName
+
+
+data class MoviesResponse(
+    @SerializedName("page") val page: Int,
+    @SerializedName("results") val results: List<Movie>,
+    @SerializedName("total_pages") val totalPages: Int,
+    @SerializedName("total_results") val totalResults: Int
+)
